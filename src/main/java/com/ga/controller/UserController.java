ac7aaa7ec46872be67480d3e24a289b1229a733c
aridoza <mendozaarielr@gmail.com>
2019-10-21 17:40:43 -0400

    added hibernate dependencies, packages

--- src/main/java/com/ga/controller/UserController.java
+++ src/main/java/com/ga/controller/UserController.java
@@ -3,7 +3,10 @@ package com.ga.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,13 +21,27 @@ public class UserController {
 		return "Hello World!!";
 	}
 	
-	@GetMapping("/users")
-	public List<User> getUsers() {
-		List<User> users = new ArrayList<User>();
+private List<User> users;
+	
+	@PostConstruct
+	public void initializeUsers() {
+		users = new ArrayList<User>();
 		
 		users.add(new User("batman", "bat", "batman@superhero.com"));
 		users.add(new User("spiderman", "spider", "spiderman@superhero.com"));
 		
+	}
+
+	@GetMapping("/users")
+	public List<User> getUsers(){
 		return users;
+	}
+	
+	@GetMapping("/users/{username}")
+	public User getUser(@PathVariable String username) {	
+		User foundUser = users.stream().filter(user -> 
+			user.getUsername().equalsIgnoreCase(username)).findFirst().orElse(null);
+		
+		return foundUser;
 	}
 }
