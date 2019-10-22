8ff1dc68604f64b1f26391eebaa5b269e64abcbd
Steven Huang <steven.charles.huang@gmail.com>
2019-10-21 20:48:12 -0400

    added seed files for easy db seeding

---
+++ seed_db.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (username, password, email)
+     VALUES ('batman', 'bat', 'bat@batmail.com'),
+            ('superman', 'super', 'super@supermail.com'),
+            ('spiderman', 'spider', 'spider@spidermail.com'),
+            ('aquaman', 'aqua', 'aqua@aquamail.com'),
+            ('galactus', 'galact', 'galact@galactmail.com');
