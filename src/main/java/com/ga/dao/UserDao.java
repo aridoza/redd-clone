30e2cd9bb79caca8d950a45d5e5a47b9141bf87a
Steven Huang <steven.charles.huang@gmail.com>
2019-10-23 19:32:37 -0400

    resolved conflicts

--- src/main/java/com/ga/dao/UserDao.java
+++ src/main/java/com/ga/dao/UserDao.java
@@ -20,5 +20,6 @@ public interface UserDao {
 	public User getUserByUsername(String username);
 
 	public User getUserByUserId(Long userId);
+
 	
 }
--- src/main/java/com/ga/dao/UserDaoImpl.java
+++ src/main/java/com/ga/dao/UserDaoImpl.java
@@ -124,4 +124,5 @@ public class UserDaoImpl implements UserDao {
 		
 		return user;
 	}
+
 }
