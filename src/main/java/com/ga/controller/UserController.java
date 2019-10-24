c35950db04275204d92cd1a98978e7a492c41155
aridoza <mendozaarielr@gmail.com>
2019-10-24 15:54:16 -0400

    added cors to all controllers

--- src/main/java/com/ga/controller/UserController.java
+++ src/main/java/com/ga/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ import com.ga.service.UserService;
 
 import antlr.Token;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/user")
 public class UserController {
