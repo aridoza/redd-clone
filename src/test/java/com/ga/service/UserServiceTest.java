628fef54322adfd05f0a2a874b1d9511577474c4
Steven Huang <steven.charles.huang@gmail.com>
2019-10-25 09:21:12 -0400

    user service test

--- src/test/java/com/ga/service/UserServiceTest.java
+++ src/test/java/com/ga/service/UserServiceTest.java
@@ -38,19 +38,23 @@ public class UserServiceTest {
         MockitoAnnotations.initMocks(this);
     }
     
+    @Before
+    public void initDummyUser() {
+    	user.setId(1L);
+    	user.setUsername("testuser");
+    	user.setPassword("testpass");
+    	
+    	System.out.println(user);
+    }
         
     @Test
     public void signup_ReturnsJwt_Success() {
         String expectedToken = "12345";
-    	
-        user.setId(1L);
-        user.setUsername("batman");
-        user.setPassword("robin");
         
         when(userDao.signup(any())).thenReturn(user);
         when(userDao.getUserByUsername(anyString())).thenReturn(user);
         when(jwtUtil.generateToken(any())).thenReturn(expectedToken);
-        when(bCryptPasswordEncoder.encode(user.getPassword())).thenReturn("robin");
+        when(bCryptPasswordEncoder.encode(user.getPassword())).thenReturn("testuser");
         
         String actualToken = userService.signup(user);
         
@@ -59,11 +63,10 @@ public class UserServiceTest {
     
     @Test
     public void signup_UserNotFound_Error() {
-        user.setId(null);
-        user.setUsername("batman");
-        user.setPassword("robin");
+        User tempUser = user;
+        tempUser.setId(null);
 
-        when(userDao.signup(any())).thenReturn(user);
+        when(userDao.signup(any())).thenReturn(tempUser);
 
         String token = userService.signup(user);
 
